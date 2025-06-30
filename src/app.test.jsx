8c04cd75fed8c6761d5d04cde3ba773f32540324
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-30 21:39:44 +0300

    Batman

---
+++ src/app.test.jsx
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+import App from './app';
+import { render, screen } from '@testing-library/react';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
